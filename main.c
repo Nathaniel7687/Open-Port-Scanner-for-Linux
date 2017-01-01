12911efeb85627d29cef537d1fe96fcfdbdc86ee
InSeob Kim <nathaniel7687@gmail.com>
2017-01-01 17:12:36 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,15 +7,13 @@ int main(int cnt, char *content[])
         int i = 0;
         char shell[1024];
 
-        if(cnt < 3)
+        if(cnt < 4)
                 exit(0);
 
-        cnt = 0;
         omp_set_num_threads(20);
         #pragma omp parallel for
-        for(i = atoi(content[1]); i < atoi(content[2]); i++) {
-                printf("No. %5d / %5d\n", i, cnt++);
-                sprintf(shell, "nmap -PN 210.119.104.155 -p%d | egrep \'closed|open\'", i);
+        for(i = atoi(content[2]); i < atoi(content[3]); i++) {
+                sprintf(shell, "nmap -PN %s -p%d | egrep \'closed|open\'", content[1], i);
                 system(shell);
         }
 
